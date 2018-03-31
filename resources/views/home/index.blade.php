30161e60e797432cafb689d061f5fa446cb7ca7e
lth-win10 <543060074@qq.com>
2018-03-31 23:48:29 +0800

    v0.24

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,7 +3,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <title>上海前景广告有限公司</title>
+    <title>海口长艺广告官方</title>
     <meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
     <link href="http://www.changemedia.com.cn/css.css" rel="stylesheet" type="text/css" />
     <link href="http://www.hainanhecai.com/base/templates/css/common.css" rel="stylesheet" type="text/css" />
