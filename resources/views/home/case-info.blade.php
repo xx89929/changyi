6e9a5922e115b629ab227dcb859030509a6b0aea
xx89929 <543060074@qq.com>
2018-06-11 21:18:45 +0800

    ch0.7

--- resources/views/home/case-info.blade.php
+++ resources/views/home/case-info.blade.php
@@ -1,4 +1,49 @@
 @extends('home.base')
 @section('content')
-    
+    <div class="about_warp">
+        <div class="shangbuk ">
+            <div class="shangbu">
+                <p>成功案列<span>/case</span></p>
+            </div>
+            <div class="qiyejianjie">
+                <a href="#" class="qiyejianjie_a" >
+                    成功案列
+                </a>
+            </div>
+            <div class="zhongbu">
+                <div class="zhongbu1" style="background: url('{{asset("images/index_28.gif")}}') no-repeat;">
+				<span>联系我们<span>
+                </div>
+            </div>
+            <div class=xiabu>
+                <h4>
+                    电话：123456<br/>
+                    联系人：李先生<br/>
+                    手机：123456789<br/>
+                    公司地址：
+                </h4>
+            </div>
+        </div>
+
+        <div class=youbu>
+            <div class=shangm>
+                <p>成功案列>详细信息</p>
+            </div>
+            <div class="xiam">
+                <div class="lianx">
+                   <img src="http://localhost/uploads/case/63a234697824fdd55bcc53db4073f6b3.jpg" >
+                </div>
+                <div class="wenzi">
+                    开了房间会计阿萨大萨达萨达阿萨大阿大撒的啊实打实打算带了房间会计阿萨大萨达萨达阿萨大阿大撒的啊实打实打算带
+                    了房间会计阿萨大萨达萨达阿萨大阿大撒的啊实打实打算带了房间会计阿萨大萨达萨达阿萨大阿大撒的啊实打实打算带
+                    了房间会计阿萨大萨达萨达阿萨大阿大撒的啊实打实打算带了房间会计阿萨大萨达萨达阿萨大阿大撒的啊实打实打算带
+                    了房间会计阿萨大萨达萨达阿萨大阿大撒的啊实打实打算带了房间会计阿萨大萨达萨达阿萨大阿大撒的啊实打实打算带
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
 @endsection
